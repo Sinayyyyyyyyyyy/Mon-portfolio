0401e30b7d02513069b063c50425ef5697abc97d
Sinayyyyyyyyyyy <yanisammour2007@gmail.com>
2026-07-17 01:01:12 +0200

    Renommage temporaire pour forcer Git

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import Navbar from './components/Navbar';
 import Hero from './components/Hero';
 import About from './components/About';
 import Skills from './components/Skills';
-import Projects from './components/Projects2';
+import Projects from './components/Projects';
 import Contact from './components/Contact';
 function App() {
 
